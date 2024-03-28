92bec23278e95ca95ebb3637e52f4821702fc46e
Laura LEFEVRE <laura.lefevre@ens.uvsq.fr>
2024-03-28 12:02:31 +0100

    fonction lecture ex code ram

--- ProjetIN620.py
+++ ProjetIN620.py
@@ -2,3 +2,17 @@
 ### LEFEVRE Laura - LE CORRE Camille ###
 ########################################
 
+### Question 1 
+
+def read_RAM(fic_in):
+    '''Fonction permettant la lecture du fichier dans laquelle le code d'une Machine RAM est stockée'''
+
+    with open(fic_in, 'r') as fic_in:
+        codeRAM = []
+        for ligne in fic_in:
+            codeRAM.append(ligne[:-1])
+
+    #print(contenu)
+    return codeRAM
+
+print(read_RAM("question1_ex code recherche max.txt"))
