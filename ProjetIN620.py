87247deabc8d9c40e84694af8b3a29289377ccc5
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2024-04-04 19:32:10 +0200

    fenetres 3 parties

--- ProjetIN620.py
+++ ProjetIN620.py
@@ -321,6 +321,7 @@ def analyse_programme(nom_fichier):
 
     # Initialisation, création du dictionnaire
     info_code_RAM(read_RAM(nom_fichier))
+    print(dico_elt_RAM)
 
     instructions = dico_elt_RAM["codeRAM"]
     historique_config = []
@@ -369,27 +370,88 @@ def analyse_programme(nom_fichier):
 
 import customtkinter
 
-def chmtFenetreP1():
-    pass
+def comptePasDiese(liste):
+    """ Renvoie le nombre d'éléments qui ne sont pas des dièses dans une liste"""
 
-def chmtFenetreP2():
-    pass
+    nb = 0
 
-def chmtFenetreP3():
-    pass
+    for elt in liste:
+        if elt != '#':
+            nb += 1
 
-app_accueil = customtkinter.CTk()
-app_accueil.geometry("420x190")
-app_accueil.title("Projet_IN-620")
+    return nb
 
-l_choisissez = customtkinter.CTkLabel(app_accueil, text="Choisissez une partie du projet")
-l_choisissez.grid(row=0, column=0)
-b_choixP1 = customtkinter.CTkButton(app_accueil, text="Simulation de l'exécution d'une machine RAM", command=chmtFenetreP1)
-b_choixP2 = customtkinter.CTkButton(app_accueil, text="Simulation d'un automate à pile avec une machine RAM", command=chmtFenetreP2)
-b_choixP3 = customtkinter.CTkButton(app_accueil, text="Optimisation de la machine RAM", command=chmtFenetreP3)
-b_choixP1.grid(row=1, column=0, padx=20, pady=10)
-b_choixP2.grid(row=2, column=0, padx=20, pady=10)
-b_choixP3.grid(row=3, column=0, padx=20, pady=10)
+def affichage_registres(listes_config_registres):
+    """ Prend en entrée une liste dont les éléments sont les dictionnaires des états
+        des registres après chaque exécution d'une instruction"""
+    
+    # Calcul de la taille maximale des registres (tous confondus)
+    #max_elts_dans_un_registre = 0
+##### dire qu'on sait comment adapter la taille et tout (discussion discord 04/04 19h)
+    #for config in listes_config_registres:
+    #    max_elts_dans_un_registre = max(max_elts_dans_un_registre, comptePasDiese(dico_elt_RAM['registres_i']))
+    #    max_elts_dans_un_registre = max(max_elts_dans_un_registre, comptePasDiese(dico_elt_RAM['registres_r']))
+    #    max_elts_dans_un_registre = max(max_elts_dans_un_registre, comptePasDiese(dico_elt_RAM['registres_o']))
+    
+    longueur_registres = 500
+    largeur_registres = 25
+
+    reg_i = customtkinter.create_rectangle(20, 20, 20+longueur_registres, 20+largeur_registres)
+    reg_r = customtkinter.create_rectangle(60, 20, 60+longueur_registres, 60+largeur_registres)
+    reg_o = customtkinter.create_rectangle(100, 20, 20+longueur_registres, 20+largeur_registres)
 
 
-app_accueil.mainloop()
+def lance_app_P1():
+
+    app_P1 = customtkinter.CTk()
+    app_P1.geometry("700x550")
+    app_P1.title("Simulation de l'exécution d'une machine RAM")
+
+    #affichage_registres([0])
+
+    app_P1.mainloop()
+
+def lance_app_P2():
+
+    app_P2 = customtkinter.CTk()
+    app_P2.geometry("700x550")
+    app_P2.title("Simulation d'un automate à pile avec une machine RAM")
+
+    app_P2.mainloop()
+
+def lance_app_P3():
+
+    app_P3 = customtkinter.CTk()
+    app_P3.geometry("700x550")
+    app_P3.title("Optimisation de la machine RAM")
+
+    app_P3.mainloop()
+
+
+def lance_app_accueil():
+
+    # Création de l'application
+    app_accueil = customtkinter.CTk()
+    app_accueil.geometry("420x190")
+    app_accueil.title("Projet_IN-620")
+
+    # Affichage du label du choix
+    l_choisissez = customtkinter.CTkLabel(app_accueil, text="Choisissez une partie du projet")
+    l_choisissez.grid(row=0, column=0)
+
+    # Affichage des 3 boutons pour basculer sur les autres applications
+    b_choixP1 = customtkinter.CTkButton(app_accueil, text="Simulation de l'exécution d'une machine RAM", command=lance_app_P1)
+    b_choixP2 = customtkinter.CTkButton(app_accueil, text="Simulation d'un automate à pile avec une machine RAM", command=lance_app_P2)
+    b_choixP3 = customtkinter.CTkButton(app_accueil, text="Optimisation de la machine RAM", command=lance_app_P3)
+    b_choixP1.grid(row=1, column=0, padx=20, pady=10)
+    b_choixP2.grid(row=2, column=0, padx=20, pady=10)
+    b_choixP3.grid(row=3, column=0, padx=20, pady=10)
+
+    app_accueil.mainloop()
+
+    return
+
+
+
+# Appel de la fonction pour démarrer l'application
+lance_app_accueil()
